f6d4d2f3ff9f2b025d854e22c8af594a840aa0f3
Joel Herb <he_qiao_2010@yeah.net>
2015-08-05 10:22:12 +0800

    Update HttpHelper.java

--- src/com/github/heqiao2010/HttpHelper.java
+++ src/com/github/heqiao2010/HttpHelper.java
@@ -13,37 +13,41 @@ import java.util.Map;
 
 /**
  * 提供处理Http请求的通用方法
- * @author joel
+ * @author heqiaoa
  *
  */
 public class HttpHelper {
+	public static final int BUFFSIZE = 2048; //缓冲区大小2M
+	public static final int TIMEOUT = 6000; //超时时间设置为6秒
+	private static String timeOutHint = "发送请求出现异常，请确保正常接入互联网！";
+	private static String streamCloseError = "关闭输入流异常！";
 	/**
 	 * 从输入流中获取byte流
 	 * @param inStream
-	 * @return
+	 * @return byte[]
 	 * @throws Exception
 	 */
 	public static byte[] readInputStream(InputStream inStream) throws Exception {
 		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-		byte[] buffer = new byte[2048];
+		byte[] buffer = new byte[BUFFSIZE];
 		int len = 0;
 		while ((len = inStream.read(buffer)) != -1) {
 			outStream.write(buffer, 0, len);
 		}
-		inStream.close();
 		return outStream.toByteArray();
 	}
 	
 	/**
-	 * 向指定URL发送GET方法的请求，返回图片
+	 * 向指定URL发送GET方法的请求，返回byte流
 	 * 
 	 * @param url
 	 *            发送请求的URL
 	 * @param param
 	 *            请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
 	 * @return URL 所代表远程资源的响应结果
+	 * @throws BusinessException 
 	 */
-	public static byte[] sendGetRetByte(String url) {
+	public static byte[] sendGetRetByte(String url) throws BusinessException {
 		InputStream in = null;
 		byte[] data = null;
 		try {
@@ -56,19 +60,22 @@ public class HttpHelper {
 			connection.setRequestProperty("connection", "Keep-Alive");
 			connection.setRequestProperty("user-agent",
 					"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+			//设置超时时间
+			connection.setConnectTimeout(TIMEOUT);
+			connection.setReadTimeout(TIMEOUT);
 			// 建立实际的连接
 			connection.connect();
 			// 获取所有响应头字段
 			Map<String, List<String>> map = connection.getHeaderFields();
 			// 遍历所有的响应头字段
+			Logger.error("Http info:");
 			for (String key : map.keySet()) {
-				System.out.println(key + "--->" + map.get(key));
+				Logger.error(key + "--->" + map.get(key));
 			}
-			InputStream inStream = connection.getInputStream();// 通过输入流获取图片数据
-			data = readInputStream(inStream);
+			in = connection.getInputStream();// 通过输入流获取图片数据
+			data = readInputStream(in);
 		} catch (Exception e) {
-			System.out.println("发送GET请求出现异常！" + e);
-			e.printStackTrace();
+			throw new BusinessException(timeOutHint + e, e);
 		}
 		// 使用finally块来关闭输入流
 		finally {
@@ -77,7 +84,7 @@ public class HttpHelper {
 					in.close();
 				}
 			} catch (Exception e2) {
-				e2.printStackTrace();
+				throw new BusinessException(streamCloseError + e2, e2);
 			}
 		}
 		return data;
@@ -92,8 +99,9 @@ public class HttpHelper {
 	 * @param param
 	 *            请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
 	 * @return URL 所代表远程资源的响应结果
+	 * @throws BusinessException 
 	 */
-	public static String sendGet(String url) {
+	public static String sendGet(String url) throws BusinessException {
 		String result = "";
 		BufferedReader in = null;
 		try {
@@ -106,25 +114,27 @@ public class HttpHelper {
 			connection.setRequestProperty("connection", "Keep-Alive");
 			connection.setRequestProperty("user-agent",
 					"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+			//设置超时时间
+			connection.setConnectTimeout(TIMEOUT);
+			connection.setReadTimeout(TIMEOUT);
 			// 建立实际的连接
 			connection.connect();
 			// 获取所有响应头字段
 			Map<String, List<String>> map = connection.getHeaderFields();
 			// 遍历所有的响应头字段
+			Logger.error("Http info:" + url);
 			for (String key : map.keySet()) {
-				System.out.println(key + "--->" + map.get(key));
+				Logger.error(key + "--->" + map.get(key));
 			}
-			// 定义 BufferedReader输入流来读取URL的响应
+			// 定义 BufferedReader输入流来读取URL的响应，注意返回的是UTF-8的格式
 			in = new BufferedReader(new InputStreamReader(
-					connection.getInputStream()));
+					connection.getInputStream(),"UTF-8"));
 			String line;
 			while ((line = in.readLine()) != null) {
 				result += line;
-//				System.out.println("line:" + line);
 			}
 		} catch (Exception e) {
-			System.out.println("发送GET请求出现异常！" + e);
-			e.printStackTrace();
+			throw new BusinessException(timeOutHint + e, e);
 		}
 		// 使用finally块来关闭输入流
 		finally {
@@ -133,9 +143,10 @@ public class HttpHelper {
 					in.close();
 				}
 			} catch (Exception e2) {
-				e2.printStackTrace();
+				throw new BusinessException(streamCloseError + e2, e2);
 			}
 		}
+		Logger.error("return:" + result);
 		return result;
 	}
 	
@@ -148,8 +159,10 @@ public class HttpHelper {
 	 * @param param
 	 *            请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
 	 * @return URL 所代表远程资源的响应结果
+	 * @throws BusinessException 
 	 */
-	public static String sendGet(String url, String param) {
+	public static String sendGet(String url, String param) 
+			throws BusinessException {
 		String urlNameString = url + "?" + param;
 		return sendGet(urlNameString);
 	}
@@ -162,8 +175,9 @@ public class HttpHelper {
 	 * @param param
 	 *            请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
 	 * @return 所代表远程资源的响应结果
+	 * @throws BusinessException 
 	 */
-	public static String sendPost(String url, String param) {
+	public static String sendPost(String url, String param) throws BusinessException {
 		PrintWriter out = null;
 		BufferedReader in = null;
 		String result = "";
@@ -176,6 +190,9 @@ public class HttpHelper {
 			conn.setRequestProperty("connection", "Keep-Alive");
 			conn.setRequestProperty("user-agent",
 					"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+			//设置超时时间
+			conn.setConnectTimeout(TIMEOUT);
+			conn.setReadTimeout(TIMEOUT);
 			// 发送POST请求必须设置如下两行
 			conn.setDoOutput(true);
 			conn.setDoInput(true);
@@ -193,8 +210,7 @@ public class HttpHelper {
 				result += line;
 			}
 		} catch (Exception e) {
-			System.out.println("发送 POST 请求出现异常！" + e);
-			e.printStackTrace();
+			throw new BusinessException(timeOutHint + e, e);
 		}
 		// 使用finally块来关闭输出流、输入流
 		finally {
@@ -206,7 +222,7 @@ public class HttpHelper {
 					in.close();
 				}
 			} catch (IOException ex) {
-				ex.printStackTrace();
+				throw new BusinessException(streamCloseError + ex, ex);
 			}
 		}
 		return result;
